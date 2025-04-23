60d091837e0b16b2b2b1563b783c2989e56c7b58
Alex <alexithor31@gmail.com>
2025-04-23 17:49:18 -0600

    se aniaden cambios a la clase carta y se crea la clase carta especial mas una prueba para las clases carta y carta especial

--- Carta.java
+++ Carta.java
@@ -22,27 +22,6 @@ public class Carta{
 	private int numero;
 	private String color;
 
-	private String convertirNumero(){
-		String numeroString = "";
-		switch(numero){
-			case 9:
-				numeroString = "Coma 2";
-			break;
-			case 10:
-				numeroString = "Coma 3";
-			break;
-			case 11:
-				numeroString = "Cancelar";
-			break;
-			case 12:
-				numeroString = "Buscar Carta";
-			break;
-			default:
-				numeroString += numero;
-		}
-		return numeroString;
-	}
-
 	// Constructor de clase
 	// Primer metodo que se ejecuta cuando se genera una instancia
 	public Carta(){
@@ -57,11 +36,6 @@ public class Carta{
 		}
 		this.color = color;
 	}
-
-	public CartaEspecial(int numero){
-		if(numero >= 0 && numero <=12){
-			this.numero = numero;
-		}
 
 	public void setNumero(int numero){
 		if(numero >= 0 && numero < 13){
@@ -82,7 +56,7 @@ public class Carta{
 	}
 
 	public void imprimir(){
-		System.out.println( convertirNumero() + " " + color);
+		System.out.println( getNumero() + " " + color);
 	}
 	
 }
